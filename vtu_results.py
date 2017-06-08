36028020689ecaf1ad1d6eadc64a013581ce5546
Sourabh Dattawad <sourabhdattawad@gmail.com>
2017-06-09 03:12:53 +0530

    Added telegram support for screenshot

--- vtu_results.py
+++ vtu_results.py
@@ -3,6 +3,11 @@
 
 # <codecell>
 
+# -*- coding: utf-8 -*-
+# <nbformat>3.0</nbformat>
+
+# <codecell>
+
 import re
 from mechanize import Browser
 import urllib2
@@ -12,15 +17,23 @@ import sys
 import os
 from stat import *
 import time
+from selenium import webdriver
+from depot.manager import DepotManager
+import telegram
+bot = telegram.Bot(token="YOUR TELEGRAM TOKEN")
+
+
+res_available = False
 
 
 def sendalert():
-    message = "Holy Shit! Results out!"
-    number = "Your-mobile-no"
+    message = "Results are out!"
+    numbers = ["PHONE NUMBER"]
+    USN = "YOUR USN"
     
     if __name__ == "__main__":    
-        username = "way2sms username(phone-no)"
-        passwd = "way2sms password"
+        username = "WAY2SMS_USERNAME"
+        passwd = "WAY2SMS_PASSWORD"
     
         message = "+".join(message.split(' '))
     
@@ -40,41 +53,52 @@ def sendalert():
         except IOError:
             print "error"
             #return()
-    
-        jession_id =str(cj).split('~')[1].split(' ')[0]
-        send_sms_url = 'http://site24.way2sms.com/smstoss.action?'
-        send_sms_data = 'ssaction=ss&Token='+jession_id+'&mobile='+number+'&message='+message+'&msgLen=136'
-        opener.addheaders=[('Referer', 'http://site25.way2sms.com/sendSMS?Token='+jession_id)]
-        try:
-            sms_sent_page = opener.open(send_sms_url,send_sms_data)
-        except IOError:
-            print "error"
-            #return()
-    
-        print "success" 
-        #return ()
+        for number in numbers:
+            
+            jession_id =str(cj).split('~')[1].split(' ')[0]
+            send_sms_url = 'http://site24.way2sms.com/smstoss.action?'
+            send_sms_data = 'ssaction=ss&Token='+jession_id+'&mobile='+number+'&message='+message+'&msgLen=136'
+            opener.addheaders=[('Referer', 'http://site25.way2sms.com/sendSMS?Token='+jession_id)]
+            try:
+                sms_sent_page = opener.open(send_sms_url,send_sms_data)
+            except IOError:
+                print "error"
+                #return()
+        
+            print "success" 
+            #return ()
+        
+	
+        depot = DepotManager.get()
+        driver = webdriver.PhantomJS()
+        driver.set_window_size(1024, 768) # set the window size that you need 
+        driver.get('http://results.vtu.ac.in/results/result_page.php?usn='+USN)
+        driver.save_screenshot('res.png')
+        bot.send_photo(chat_id="telegram group-chat id", photo=open('res.png', 'rb'))
 
         
 def checkresult():
-    
-    #use http://results.vtu.ac.in/vitavireval.php **for revaluation results**
-    #use http://results.vtu.ac.in **for regular results**
+    browser = Browser()
+    content = ''
     try:
-        browser = Browser()
-        browser.open("http://results.vtu.ac.in/vitavireval.php") 
-        browser.select_form(nr=0)
-        browser['rid'] = 'YOUR-USN'
-        response = browser.submit()
-        content = response.read()
+        rep = browser.open("http://results.vtu.ac.in/results/result_page.php?usn="+USN)
+        content = rep.read()
+         
     except:
-        content = 'Results are not yet available'
-    if('Results are not yet available' not in content):
+        content = 'not available'
+        
+        
+    if('not available' not in content):
         res_available = True
         sendalert()
+        
 
+count = 0
 while True:
     checkresult()
-    time.sleep(30)
+    count+=1
+    print count
+    time.sleep(10)
 
     
 
@@ -85,4 +109,6 @@ while True:
 
 
     
+
+# <codecell>
 
